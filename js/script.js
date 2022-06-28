6ee1e3705f7c2a82c01686d110bfbc77115a4d02
Stelko <stilyanmtoskov@gmail.com>
2022-06-28 21:41:00 +0100

    pageLoad function + hide otherJobRole on page load

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
+function pageLoad() {
+    nameField.focus();
+    otherJobRole.style.display = "none";
+}
+
 let bodyHTML = document.getElementsByTagName("body")[0];
 let nameField = document.getElementById("name");
+let userTitle = document.getElementById("title");
+let otherJobRole = document.getElementById("other-job-role");
 // When page loads, the name input field is selected by default
 bodyHTML.addEventListener(
     "load",
-    nameField.focus()
+    pageLoad()
 );
